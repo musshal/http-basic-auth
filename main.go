476fde2a33a44edb0f068a538f1cb7d1f5e52a9b
Musthafa Faishal <musthafafaishal@gmail.com>
2022-10-14 15:27:52 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+func main() {
+	http.HandleFunc("/student", ActionStudent)
+
+	server := new(http.Server)
+	server.Addr = ":9000"
+
+	fmt.Println("Server started at localhost:9000")
+	server.ListenAndServe()
+}
+
+func ActionStudent(w http.ResponseWriter, r *http.Request) {
+	if !Auth(w, r) {
+		return
+	}
+	if !AllowOnlyGET(w, r) {
+		return
+	}
+
+	if id := r.URL.Query().Get("id"); id != "" {
+		OutputJSON(w, SelectStudent(id))
+		return
+	}
+
+	OutputJSON(w, GetStudent())
+}
+
+func OutputJSON(w http.ResponseWriter, o interface{}) {
+	res, err := json.Marshal(o)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
